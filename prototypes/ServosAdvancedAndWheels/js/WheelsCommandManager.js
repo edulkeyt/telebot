20010ffb91e78f7ba5a7ada825176eacc600d044
edulkeyt <edulkeyt@yandex.ru>
2017-09-15 16:50:22 +0600

    some improvements

--- prototypes/ServosAdvancedAndWheels/js/WheelsCommandManager.js
+++ prototypes/ServosAdvancedAndWheels/js/WheelsCommandManager.js
@@ -1,14 +1,51 @@
-function wheels(btnRightFwd, btnRightBck, btnLeftFwd, btnLeftBck, wheelsFunction){
+function testWheelState(){
 	
-	function wheelState(){
-		return {
-			forward: 1,
-			stop: 0,
-			back: 2,
-			notChanged: 3
-		};
+	function testStop(){
+		return [wheelState().is(0).stop === true, wheelState().is(1).stop === false, wheelState().is(2).stop === false, wheelState().is(3).stop === false];
 	}
 	
+	function testForward(){
+		return [wheelState().is(0).forward === false, wheelState().is(1).forward === true, wheelState().is(2).forward === false, wheelState().is(3).forward === false];
+	}
+		
+	function testBack(){
+		return [wheelState().is(0).back === false, wheelState().is(1).back === false, wheelState().is(2).back === true, wheelState().is(3).back === false];
+	}
+	
+	function testNotChanged(){
+		return [wheelState().is(0).notChanged === false, wheelState().is(1).notChanged === false, wheelState().is(2).notChanged === false, wheelState().is(3).notChanged === true];
+	}
+	
+	return [testStop(), testForward(), testBack(), testNotChanged()];
+}
+
+function wheelState(){
+	const forwardConst = 1;
+	const backConst = 2;
+	const stopConst = 0;
+	const notChangedConst = 3;
+	
+	return {
+		forward: 1,
+		stop: 0,
+		back: 2,
+		notChanged: 3,
+		is: function(oneWheelState){
+			return {
+				forward: oneWheelState === forwardConst,
+				back: oneWheelState === backConst,
+				stop: oneWheelState === stopConst,
+				notChanged: oneWheelState === notChangedConst
+			};
+		}
+	};
+}
+
+function getRightWheelState(wheelsState){ return wheelsState%4; }
+function getLeftWheelState(wheelsState){ return Math.floor(wheelsState/4); }
+
+function wheels(btnRightFwd, btnRightBck, btnLeftFwd, btnLeftBck, wheelsFunction){
+		
 	function wheelCommandString(rightWheelState, leftWheelState){
 		return (rightWheelState + leftWheelState * 4).toString();
 	}
