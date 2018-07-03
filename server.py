e9066cef96644966c0ff80be77434ae7f411d2ba
edulkeyt <edulkeyt@luxoft.com>
2018-07-03 10:35:59 +0600

    servos logic separated from server

--- server.py
+++ server.py
@@ -1,14 +1,8 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 import time
 import urllib
-import Adafruit_PCA9685
-import RPi.GPIO as gpio
 from wheels.py import WheelsController
-
-SG90_MAX_ANGLE = 180;
-
-SG90_MIN_PULSE = 130
-SG90_PULSE_DELTA = 470
+from servos.py import ServosController
 
 SERVO_COMMAND_PARAMETER_NAME = "servos=";
 WHEELS_COMMAND_PARAMETER_NAME = "wheels=";
@@ -22,8 +16,6 @@ WHEELS_ARGUMENTS_SEPARATOR = ":";
 
 SERVER_ADDRESS = ("", 8000)
 
-PCA9685_FREQUENCY = 60
-
 class MyHandler(CGIHTTPRequestHandler):
 
     def setHeaders(self):
@@ -35,12 +27,6 @@ class MyHandler(CGIHTTPRequestHandler):
         self.setHeaders();
     
     def do_GET(self):
-
-        def setServosPositionsFromDegreesStrings(strings):
-            for i, substring in enumerate(strings):
-                angle = int(substring);
-                pulse = int(SG90_MIN_PULSE + angle * SG90_PULSE_DELTA / SG90_MAX_ANGLE);
-                pca9685.set_pwm(i, 0, pulse);
 
         command = self.path[1:];        
 
@@ -90,8 +76,8 @@ class MyHandler(CGIHTTPRequestHandler):
     def buildServoSetting(self, settingSubstr, servoAngle):
         return '"angle":' + str(servoAngle) + settingSubstr[settingSubstr.find(','):]
 
-pca9685 = Adafruit_PCA9685.PCA9685()
-pca9685.set_pwm_freq(PCA9685_FREQUENCY)
+servos = ServosController();
+servos.init();
 
 wheels = WheelsController();
 wheels.init();
